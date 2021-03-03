2600269387b686fa11bd654b3f0df5a0006349c9
Facundo Torraca <ftorraca@fi.uba.ar>
2021-03-03 09:22:18 -0300

    chnage index

--- src/index.js
+++ src/index.js
@@ -9,11 +9,14 @@ import * as serviceWorker from './serviceWorker';
 
 import { icons } from './assets/icons';
 
-import { Provider } from 'react-redux';
-import store from './store';
+//import { Provider } from 'react-redux';
+//import store from './store';
 
 React.icons = icons;
 
+ReactDOM.render(<App />, document.getElementById('root'));
+
+/*
 ReactDOM.render(
   <Provider store={store}>
     <App />
@@ -21,6 +24,7 @@ ReactDOM.render(
   document.getElementById('root'),
 );
 
+*/
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
